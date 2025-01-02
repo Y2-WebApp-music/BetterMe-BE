e31e61e7417031b8dd5c1b8a1c54093fedbae7f8
Ariez <kritamet.cho@gmail.com>
2025-01-02 21:57:52 +0700

    add config env

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import { Elysia } from "elysia";
+import mongoose from "mongoose";
+import { PORT, MONGO_URI } from "../config/env";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.get("/", () => "Hello Elysia");
+
+app.listen(PORT, async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log(`🦊 Elysia is running at http://localhost:${PORT}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
