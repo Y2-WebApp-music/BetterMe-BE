7528cded13c34113b8c0a02f9271110f6ea0cf13
Ariez <kritamet.cho@gmail.com>
2025-01-15 21:57:19 +0700

    ✅ update user

--- src/Controller/user/UserController.ts
+++ src/Controller/user/UserController.ts
@@ -1,5 +1,6 @@
 import { Elysia } from "elysia";
-import { UserModel } from "../../Model/User";
+import { User, UserModel } from "../../Model/User";
+import BMR_calculate from "../../utils/BMR";
 
 const app = new Elysia();
 
@@ -21,6 +22,46 @@ export const getUserById = app.get("/profile/:uid", async ({ params }) => {
         }
 
         return user_data;
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+export const updateUser = app.put("/update/:id", async (
+    { params, body }: {
+        params: { id: string },
+        body: User
+    }
+) => {
+    try {
+        const { id } = params;
+        const updateFields = body;
+
+        const user = await UserModel.findById(id);
+        if (!user) {
+            return { message: "User not found" };
+        }
+
+        if (updateFields.birth_date || updateFields.gender || updateFields.weight || updateFields.height || updateFields.activity) {
+            const birth_date = updateFields.birth_date || user.birth_date;
+            const gender = updateFields.gender || user.gender;
+            const weight = updateFields.weight || user.weight;
+            const height = updateFields.height || user.height;
+            const activity = updateFields.activity || user.activity;
+
+            const calorie_need = BMR_calculate({ birth_date, gender, weight, height, activity });
+            updateFields.calorie_need = calorie_need;
+        }
+
+        Object.assign(user, updateFields);
+        await user.save();
+
+        return {
+            message: "Update user success",
+            user
+        };
     } catch (error) {
         console.log(error);
     }
--- src/Routes/userRoute.ts
+++ src/Routes/userRoute.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 import { register, login, logout } from "../Controller/user/AuthController";
-import { getUserById } from "../Controller/user/UserController";
+import { getUserById, updateUser } from "../Controller/user/UserController";
 
 const router = new Elysia().group("/user", (app) =>
     app
@@ -9,6 +9,8 @@ const router = new Elysia().group("/user", (app) =>
         .use(logout) // POST /user/logout
         
         .use(getUserById) // GET /user/profile/:uid
+
+        .use(updateUser) // PUT /user/update/:id
 );
 
 export default router;
