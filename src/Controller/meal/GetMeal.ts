05bcce52efdbc4ccdbe1d9d9cae82792376248cb
Ariez <kritamet.cho@gmail.com>
2025-01-17 19:37:35 +0700

    ✅ get meal detail

---
+++ src/Controller/meal/GetMeal.ts
@@ -0,0 +1,32 @@
+import { Elysia } from "elysia";
+import { Meal, MealModel } from "../../Model/Meal";
+
+const app = new Elysia();
+
+export const getMealDatail = app.get("/detail/:meal_id", async ({ params }) => {
+    try {
+        const { meal_id } = params;
+        const meal = await MealModel.findById(meal_id).populate('create_by', 'firebase_uid username');
+        if (!meal) {
+            return { message: "Meal not found" };
+        }
+
+        const meal_data = {
+            meal_id: meal._id.toString(),
+            create_by: meal.create_by,
+            meal_date: meal.meal_date,
+            food_name: meal.food_name,
+            image: meal.image,
+            portion: meal.portion,
+            calorie: meal.calorie,
+            protein: meal.protein,
+            carbs: meal.carbs,
+            fat: meal.fat,
+            createByAI: meal.createByAI
+        }
+        
+        return meal_data;
+    } catch (error) {
+        console.log(error);
+    } 
+});
--- src/Routes/mealRoute.ts
+++ src/Routes/mealRoute.ts
@@ -1,9 +1,12 @@
 import { Elysia } from "elysia";
-import { addMealByUser } from "../Controller/meal/MealController";
+import { addMealByUser } from "../Controller/meal/AddMeal";
+import { getMealDatail } from "../Controller/meal/GetMeal";
 
 const mealRoute = new Elysia().group("/meal", (app) =>
     app
         .use(addMealByUser) // POST /meal/by-user
+
+        .use(getMealDatail) // GET /detail/:meal_id
 );
 
 export default mealRoute;
