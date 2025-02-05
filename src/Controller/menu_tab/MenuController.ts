dfc562b77a2eb3e78cf62d4aeee786fdd2ff35a0
Ariez <kritamet.cho@gmail.com>
2025-02-05 17:37:41 +0700

    ✅ count goals in menu tab
    count status: completed, in-progress, failed

--- src/Controller/menu_tab/MenuController.ts
+++ src/Controller/menu_tab/MenuController.ts
@@ -72,3 +72,48 @@ export const getWeeklyMeal = app.get("/meal/weekly/:date", async ({ jwt, cookie:
         console.log(error);
     }
 });
+
+
+
+// Count goals status of user
+export const countGoals = app.get("/goal/count", async ({ jwt, cookie: { token } }) => {
+    try {
+        const validToken = await jwt.verify(token.value);
+        if (!validToken) {
+            return { message: "Invalid token" };
+        }
+        const user_id = validToken.user_id;
+
+        const goals = await GoalModel.find({ create_by: user_id });
+
+        const completeGoals = goals.filter(goal => {
+            const completeTaskCount = goal.tasks.filter(task => task.status === true).length;
+            return completeTaskCount === goal.tasks.length;
+        });
+
+        const inProgressGoals = goals.filter(goal => {
+            const today = new Date();
+            const end_date = new Date(goal.end_date);
+            end_date.setDate(end_date.getDate() + 1);
+            const completeTaskCount = goal.tasks.filter(task => task.status === true).length;
+            return (completeTaskCount < goal.tasks.length) && (today <= end_date);
+        });
+
+        const failGoals = goals.filter(goal => {
+            const today = new Date();
+            const end_date = new Date(goal.end_date);
+            end_date.setDate(end_date.getDate() + 1);
+            const completeTaskCount = goal.tasks.filter(task => task.status === true).length;
+            return (completeTaskCount < goal.tasks.length) && (today > end_date);
+        });
+
+
+        return {
+            completed: completeGoals.length,
+            in_progress: inProgressGoals.length,
+            failed: failGoals.length,
+        };
+    } catch (error) {
+        console.log(error);
+    }
+});
--- src/Routes/menuRoute.ts
+++ src/Routes/menuRoute.ts
@@ -1,9 +1,10 @@
 import { Elysia } from "elysia";
-import { getWeeklyMeal } from '../Controller/menu_tab/MenuController';
+import { getWeeklyMeal, countGoals } from '../Controller/menu_tab/MenuController';
 
 const menuRoute = new Elysia().group("/menu", (app) =>
     app
         .use(getWeeklyMeal) // GET /menu/meal/weekly/:date
+        .use(countGoals) // GET /menu/goal/count
 );
 
 export default menuRoute;
