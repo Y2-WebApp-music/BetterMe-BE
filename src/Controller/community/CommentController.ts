980e89fcedf72c703dfc6de88f0120f192245119
Ariez <kritamet.cho@gmail.com>
2025-02-22 23:56:46 +0700

    ✅ Community: delete comment

--- src/Controller/community/CommentController.ts
+++ src/Controller/community/CommentController.ts
@@ -5,6 +5,7 @@ const app = new Elysia();
 
 interface CommentBody {
     post_id: string;
+    comment_id: string;
     content: string;
     create_by: string;
 }
@@ -34,6 +35,36 @@ export const createComment = app.post("/comment/create", async ({ body }: { body
         return {
             message: "Create comment success",
             comment
+        };
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+export const deleteComment = app.delete("/comment/delete", async ({ body }: { body: CommentBody }) => {
+    try {
+        const {
+            post_id,
+            comment_id
+        } = body;
+
+        const post = await PostModel.findById(post_id);
+        if (!post) {
+            return { message: "Post not found" };
+        }
+
+        const comment = await CommentModel.findById(comment_id);
+        if (!comment) {
+            return { message: "Comment not found" };
+        }
+
+        await comment.deleteOne();
+        await post.updateOne({ $pull: { comment: comment_id } });
+
+        return {
+            message: "Delete comment success",
         };
     } catch (error) {
         console.log(error);
--- src/Routes/communityRoute.ts
+++ src/Routes/communityRoute.ts
@@ -1,12 +1,13 @@
 import { Elysia } from "elysia";
 import { createPost } from "../Controller/community/PostController";
-import { createComment } from "../Controller/community/CommentController";
+import { createComment, deleteComment } from "../Controller/community/CommentController";
 
 const communityRoute = new Elysia().group("/community", (app) =>
     app
         .use(createPost) // POST /community/post/create
 
         .use(createComment) // POST /community/comment/create
+        .use(deleteComment) // DELETE /community/comment/delete
 );
 
 export default communityRoute;
