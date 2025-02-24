11ee869f13aa2df69c004a03bfd34679f43281de
Ariez <kritamet.cho@gmail.com>
2025-02-24 18:12:13 +0700

    ✅ follow/unfollow user

--- src/Controller/community/LikeFollow.ts
+++ src/Controller/community/LikeFollow.ts
@@ -33,3 +33,42 @@ export const likePost = app.put("/post/like", async ({ query }) => {
         console.log(error);
     }
 });
+
+
+
+export const followUser = app.put("/user/follow", async ({ query }) => {
+    try {
+        const { user_id, follower_id } = query;
+
+        if (!user_id || !follower_id) {
+            return { message: "Missing user_id or follower_id" };
+        }
+
+        const user1 = await UserModel.findById(user_id);
+        if (!user1) {
+            return { message: "User not found" };
+        }
+
+        const user2 = await UserModel.findById(follower_id);
+        if (!user2) {
+            return { message: "User not found (follower)" };
+        }
+
+        const alreadyFollow = user1.follower.find((follower) => follower.toString() === follower_id);
+        if (alreadyFollow) {
+            await user1.updateOne({ $pull: { follower: follower_id } });
+            await user2.updateOne({ $pull: { following: user_id } });
+            return {
+                message: "Unfollow success",
+            }
+        } else {
+            await user1.updateOne({ $push: { follower: follower_id } });
+            await user2.updateOne({ $push: { following: user_id } });
+            return {
+                message: "Follow success",
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
--- src/Routes/communityRoute.ts
+++ src/Routes/communityRoute.ts
@@ -2,7 +2,7 @@ import { Elysia } from "elysia";
 import { createPost } from "../Controller/community/CreatePost";
 import { deletePost } from "../Controller/community/DeletePost";
 import { createComment, deleteComment } from "../Controller/community/CommentController";
-import { likePost } from "../Controller/community/LikeFollow";
+import { likePost, followUser } from "../Controller/community/LikeFollow";
 
 const communityRoute = new Elysia().group("/community", (app) =>
     app
@@ -13,6 +13,7 @@ const communityRoute = new Elysia().group("/community", (app) =>
         .use(deleteComment) // DELETE /community/comment/delete?post_id=&comment_id=
 
         .use(likePost) // PUT /community/post/like?post_id=&user_id=
+        .use(followUser) // PUT /community/user/follow?user_id=&follower_id=
 );
 
 export default communityRoute;
