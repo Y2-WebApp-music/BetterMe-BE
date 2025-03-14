5b522fd90f4cfd655bda19f46b423f5b91ddb1ef
Ariez <kritamet.cho@gmail.com>
2025-03-14 21:02:37 +0700

    ♻️ search post

--- src/Controller/community/Search.ts
+++ src/Controller/community/Search.ts
@@ -12,12 +12,27 @@ interface SearchBody {
 export const searchPost = app.post("/search/post", async ({ body }: { body: SearchBody }) => {
     try {
         const { keyword } = body;
-        
+
         const posts = await PostModel.find({
             content: { $regex: keyword, $options: "i" }
-        });
+        }).populate("create_by", "username profile_img");
 
-        return posts;
+        const post_data = posts.map(post => {
+            const likeCount = post.like.length;
+            const commentCount = post.comment.length;
+            return {
+                post_id: post._id.toString(),
+                create_by: post.create_by,
+                date: post.post_date,
+                content: post.content,
+                tag: post.tag,
+                image: post.image_url,
+                like: likeCount,
+                comment: commentCount,
+            };
+        })
+
+        return post_data;
     } catch (error) {
         console.log(error);
         return error;
@@ -29,12 +44,27 @@ export const searchPost = app.post("/search/post", async ({ body }: { body: Sear
 export const searchPostByTag = app.post("/search/tag", async ({ body }: { body: SearchBody }) => {
     try {
         const { tag } = body;
-        
+
         const posts = await PostModel.find({
             tag: { $in: tag }
-        });
+        }).populate("create_by", "username profile_img");
 
-        return posts;
+        const post_data = posts.map(post => {
+            const likeCount = post.like.length;
+            const commentCount = post.comment.length;
+            return {
+                post_id: post._id.toString(),
+                create_by: post.create_by,
+                date: post.post_date,
+                content: post.content,
+                tag: post.tag,
+                image: post.image_url,
+                like: likeCount,
+                comment: commentCount,
+            };
+        })
+
+        return post_data;
     } catch (error) {
         console.log(error);
     }
@@ -45,7 +75,7 @@ export const searchPostByTag = app.post("/search/tag", async ({ body }: { body: 
 export const searchGoal = app.post("/search/goal", async ({ body }: { body: SearchBody }) => {
     try {
         const { keyword } = body;
-        
+
         const goals = await GoalModel.find({
             goal_name: { $regex: keyword, $options: "i" }
         });
