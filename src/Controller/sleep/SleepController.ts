46547bd4f40745fcd544a1f8cd2cfbab002476c9
Ariez <kritamet.cho@gmail.com>
2025-03-16 22:33:24 +0700

    ✅ update sleep

--- src/Controller/sleep/SleepController.ts
+++ src/Controller/sleep/SleepController.ts
@@ -30,3 +30,29 @@ export const createSleep = app.post("/create", async ({ body }: { body: Sleep })
         console.log(error);
     }
 });
+
+
+
+export const updateSleep = app.put("/update/:sleep_id", async (
+    { params, body }: {
+        params: { sleep_id: string },
+        body: Sleep
+    }
+) => {
+    try {
+        const { sleep_id } = params;
+        const updateFields = body;
+
+        const sleep = await SleepModel.findByIdAndUpdate(sleep_id, updateFields, { new: true });
+        if (!sleep) {
+            return { message: "Sleep not found" };
+        }
+
+        return {
+            message: "Update sleep success",
+            sleep
+        };
+    } catch (error) {
+        console.log(error);
+    }
+});
--- src/Routes/sleepRoute.ts
+++ src/Routes/sleepRoute.ts
@@ -1,9 +1,10 @@
 import { Elysia } from "elysia";
-import { createSleep } from "../Controller/sleep/SleepController";
+import { createSleep, updateSleep } from "../Controller/sleep/SleepController";
 
 const sleepRoute = new Elysia().group("/sleep", (app) =>
     app
-        .use(createSleep)
+        .use(createSleep) // POST /sleep/create
+        .use(updateSleep) // PUT /sleep/update/:sleep_id
 );
 
 export default sleepRoute;
